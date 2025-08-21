cce83a7e73bc70b41c6c79435cd6129edbd8cf4b
Hieu Bui Vinh <buivinhhieu217@gmail.com>
2025-08-22 00:17:52 +0700

    Inital commit

---
+++ src/main/java/org/example/xphrtestingassignment/XphrTestingAssignmentApplication.java
@@ -0,0 +1,13 @@
+package org.example.xphrtestingassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XphrTestingAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XphrTestingAssignmentApplication.class, args);
+    }
+
+}
